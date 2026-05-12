cfd94ce84c0da4ba82ad59433dad8a21638d634d
Sunk <terunya02@gmail.com>
2026-05-12 05:03:04 -0400

    Fix restart.py

--- restart.py
+++ restart.py
@@ -1,9 +1,9 @@
 import docker
 
 client = docker.from_env()
-container = client.containers.get("sm64coopdx")
+container = client.containers.get("coop-server-1")
 
-for raw_line in container.logs(stream=True, follow=True, since=0):
+for raw_line in container.logs(stream=True, follow=True):
     line = raw_line.decode("utf-8", errors="replace")
 
     if "Coopnet shutdown!" in line and "packet_chat.c" not in line:

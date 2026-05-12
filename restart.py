87c4766fc5a7c3868c11dece7e40a3ad17cb4ec3
Sunk <terunya02@gmail.com>
2026-05-12 14:13:56 -0400

    Fix restart and logging

--- restart.py
+++ restart.py
@@ -1,12 +1,23 @@
 import docker
 
-client = docker.from_env()
-container = client.containers.get("coop-server-1")
+client: docker.DockerClient = docker.from_env()
 
-for raw_line in container.logs(stream=True, follow=True):
-    line = raw_line.decode("utf-8", errors="replace")
+def line_generator(stream):
+    line = ""
+    for raw_line in stream:
+        char = raw_line.decode("utf-8", errors="replace")
+        line += char
+        if char == "\n":
+            yield line
+            line = ""
 
-    if "Coopnet shutdown!" in line and "packet_chat.c" not in line:
-        print("CoopNet disconnected; restarting container")
-        container.restart()
-        break
+with open("logs.txt", "a") as file:
+    while True:
+        container = client.containers.get("sm64coopdx-server")
+        for line in line_generator(container.logs(stream=True, follow=True)):
+            if "packet_chat.c" in line or ("coopnet.c" in line and "Coopnet shutdown!" in line):
+                file.write(line)
+                file.flush()
+
+            if "Coopnet shutdown!" in line and "packet_chat.c" not in line:
+                container.restart()
